d146dec83986d99a17f35c5c27fd09a1cebd1f78
rio <riogoodman@gmail.com>
2017-05-15 00:03:29 -0700

    gradient descent done

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,8 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-    theta = theta - alpha * 2 * X * ( X * theta - y )
 
+    grad = X' * (X * theta - y);
+    theta = theta - (alpha / m)*grad;
 
 
 
