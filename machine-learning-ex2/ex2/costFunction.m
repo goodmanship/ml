e960ec8f6adc6dbc370ef7aab24afebb8a1ccb3c
rio <riogoodman@gmail.com>
2017-05-15 22:39:37 -0700

    sigmoid cost fcn + gradient

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -20,6 +20,9 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+grad = (1 / m) .* X' * (sigmoid(X * theta) - y);
+
+J = (1 / m) * (-y' * log(sigmoid(X * theta)) - (1 - y)' * (log(1 - sigmoid(X * theta))));
 
 
 
